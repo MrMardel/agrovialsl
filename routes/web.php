b854fb1c46f72dd0d55d287c763237541d8e326b
Franco Casas <francomcas@Francos-MacBook-Pro.local>
2025-07-22 11:51:00 -0300

    fix: graphql endpoint

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{any}', function () {
     return file_get_contents(public_path('index.html'));
 })->where('any', '.*');
+
+Route::post('/test-graphql', function (Illuminate\Http\Request $request) {
+    \Log::info('Cuerpo recibido en test-graphql', $request->all());
+    return response()->json($request->all());
+});
